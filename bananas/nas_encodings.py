c4fa7088a6c3fb1feac43d8dc055ff16b7130ec8
B-singularity <skil1489@naver.com>
2025-03-06 01:19:59 +0900

    refactor: bananas_adj_encoding

--- bananas/nas_encodings.py
+++ bananas/nas_encodings.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def adj_encoding(matrix, ops):
+def adj_encoding(matrix, ops, ops_dic):
     # encoding
     num_vertices = matrix.shape[0]
-    ops_arr = np.array(list(ops.values()))
+    ops_arr = np.vectorize(lambda op: ops_dic[op])(ops)
     upper_triangle = np.triu_indices(num_vertices, 1)
     upper_vals = matrix[upper_triangle]
 
-    encoding_length = (num_vertices * (num_vertices - 1)) // 2 + len(ops)
+    encoding_length = (num_vertices * (num_vertices - 1)) // 2 + (num_vertices - 2)
     encoding = np.zeros(encoding_length)
 
     encoding[:len(upper_vals)] = upper_vals
@@ -15,12 +15,7 @@ def adj_encoding(matrix, ops):
 
     return encoding
 
+def cat_adj_encoding(matrix, ops):
+    num_vertices = matrix.shape[0]
+    encoding_length = (num_vertices * (num_vertices - 1)) // 2 + (num_vertices - 2)
 
-matrix = np.random.randn(4, 4)
-upper_triangle = np.triu_indices(4, 1)
-encoding = np.zeros(8)
-ops = {'CONV_3X3':10, 'CONV_5X5':20, 'POOL_3X3':30}
-
-# print(matrix)
-# print(upper_triangle)
-# print(matrix[upper_triangle])
--- tests/bananas/test_encodings.py
+++ tests/bananas/test_encodings.py
@@ -9,9 +9,10 @@ class MyTestCase(unittest.TestCase):
             [1, 0, 3],
             [2, 3, 0]
         ])
-        ops = {'CONV_3X3':10, 'POOL_3X3':30, 'CONV_5X5':5, 'POOL_5X5':5}
-        expected_encoding = np.array([1, 2, 3, 10, 30, 5, 5])
-        result = encoding.adj_encoding(matrix, ops)
+        ops_dic = {'CONV_3X3':10, 'POOL_3X3':30, 'CONV_5X5':5, 'POOL_5X5':5}
+        expected_encoding = np.array([1, 2, 3, 10])
+        ops = ['CONV_3X3']
+        result = encoding.adj_encoding(matrix, ops, ops_dic)
         np.testing.assert_array_equal(result, expected_encoding)
 
 
